509df59340eeaa0673d6579c3c58e29982cc9aff
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-30 22:47:39 +0300

    render something

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -15,6 +15,7 @@ app.set('view engine', 'pug');
 app.route('/')
   .get((req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login', showLogin: true });
   });
 
 app.listen(process.env.PORT || 3000, () => {
