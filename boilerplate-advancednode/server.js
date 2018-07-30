5f517b3294de58765e6ae7b7767a7ab1d00da934
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-30 23:07:36 +0300

    Set up Passport Not Passed

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -3,6 +3,8 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
@@ -11,13 +13,22 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
-
 app.route('/')
   .get((req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
-    res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login', showLogin: true });
+  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login', showLogin: true });
   });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
